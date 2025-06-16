990ddc4b5c98b252fccb0c9aaddf5b755f476986
rob-hayward <hayward.m.rob@gmail.com>
2025-06-16 11:11:01 +0100

    added verbose logging to debug auth0 flow

--- ProjectZer0Backend/src/main.ts
+++ ProjectZer0Backend/src/main.ts
@@ -10,7 +10,9 @@ import { AllExceptionsFilter } from './all-exceptions.filter';
 import { Neo4jExceptionFilter } from './neo4j/neo4j-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'], // Enable all log levels
+  });
   const configService = app.get(ConfigService);
   const logger = new Logger('Bootstrap');
 
